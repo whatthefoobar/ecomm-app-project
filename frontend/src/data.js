bc877a0d60901f459f40925f7b48b0756ca866c6
whatthefoobar <irina.mntn@gmail.com>
2022-03-21 17:54:55 +0100

    added 4 test products in app.js

---
+++ frontend/src/data.js
@@ -0,0 +1,53 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Hawaii Slim shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpeg', // 679px × 829px
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Adidas Fit Shirt',
+      slug: 'adidas-fit-shirt',
+      category: 'Shirts',
+      image: '/images/p2.jpeg',
+      price: 250,
+      countInStock: 20,
+      brand: 'Adidas',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      name: 'Nike Cargo Pant',
+      slug: 'nike-cargo-pant',
+      category: 'Pants',
+      image: '/images/p3.jpeg',
+      price: 25,
+      countInStock: 15,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality product',
+    },
+    {
+      name: 'Asos Fit Pant',
+      slug: 'asos-fit-pant',
+      category: 'Pants',
+      image: '/images/p4.jpeg',
+      price: 65,
+      countInStock: 5,
+      brand: 'Asos',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+  ],
+};
+export default data;
