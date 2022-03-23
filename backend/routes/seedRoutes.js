bb5a21658f827c2afdbe651bcf5246e4df1e5ce3
whatthefoobar <irina.mntn@gmail.com>
2022-03-23 16:07:11 +0100

    created a users  dbon the backend

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import Product from '../models/productModel.js';
 import data from '../data.js';
+import User from '../models/userModel.js';
 
 const seedRouter = express.Router();
 
 seedRouter.get('/', async (req, res) => {
   await Product.deleteMany({}); // remove all previous records in the product model in the db
   const createdProducts = await Product.insertMany(data.products);
-  res.send({ createdProducts }); // sends products to the frontend
+  await User.remove({});
+  const createdUsers = await User.insertMany(data.users);
+  res.send({ createdProducts, createdUsers }); // sends products, users to the frontend
 });
 export default seedRouter;
