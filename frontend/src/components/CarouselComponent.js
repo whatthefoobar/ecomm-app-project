aa78446444aede1825b178faf296fac52959e8f9
whatthefoobar <irina.mntn@gmail.com>
2022-03-27 19:13:22 +0200

    ugly messy style but Link works on the Explore btn

--- frontend/src/components/CarouselComponent.js
+++ frontend/src/components/CarouselComponent.js
@@ -1,53 +1,60 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import Container from 'react-bootstrap/esm/Container';
+// import Container from 'react-bootstrap/esm/Container';
+import { Link } from 'react-router-dom';
 
 const CarouselComponent = () => {
   return (
-    <Container>
-      <Carousel variant="dark" fade>
-        <Carousel.Item>
-          <img
-            className="d-block w-100 height"
-            src="/images/home221.jpg"
-            alt="First slide"
-          />
-          <Carousel.Caption className="caption-color">
-            <h3>Find your signiture style</h3>
-            <p>Explore our latest threads</p>
-            <button className="btn btn-primary">Explore</button>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100 height"
-            src="/images/home.jpeg"
-            alt="Second slide"
-          />
+    <Carousel variant="dark" fade>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 height"
+          src="/images/home221.jpg"
+          alt="First slide"
+        />
+        <Carousel.Caption className="caption-color">
+          <h3>Find your signiture style</h3>
+          <p>Explore our latest threads</p>
+          <Link to="/products" className="btn btn-primary">
+            Explore
+          </Link>
+        </Carousel.Caption>
+      </Carousel.Item>
 
-          <Carousel.Caption className="caption-color">
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <button className="btn btn-primary">Explore</button>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100 height"
-            src="/images/home5.jpg"
-            alt="Third slide"
-          />
+      <Carousel.Item>
+        <img
+          className="d-block w-100 height"
+          src="/images/home.jpeg"
+          alt="Second slide"
+        />
 
-          <Carousel.Caption className="caption-color">
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-            <button className="btn btn-primary">Explore</button>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    </Container>
+        <Carousel.Caption className="caption-color">
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <Link to="/products" className="btn btn-primary">
+            Explore
+          </Link>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100 height"
+          src="/images/home5.jpg"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption className="caption-color">
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+          <Link to="/products" className="btn btn-primary">
+            Explore
+          </Link>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 };
 
